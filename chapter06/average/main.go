50650185f19bbe567f7b4191f70cdd1afe318fbf
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-12-11 20:41:47 +1000

    Amplement the average program

---
+++ chapter06/average/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"hf-go/chapter06/datafile"
+	"log"
+	"os"
+)
+
+func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(path)
+	numbers, err := datafile.GetFloats("chapter06/average/data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
